2d01c8f3e684b7fd4d9de3537fff6e23c1a3a5c0
Beatriz <beatrizandreosbezerra1@gmail.com>
2025-06-26 11:35:36 -0300

    Adicionando Aula 3 e atividades

---
+++ Aula 3/Exercicios/novo.py
@@ -0,0 +1,16 @@
+quantidade = 0
+total_alunos = 0
+turma_atual = 0
+entrada = ""
+eh_numero = 0
+indice = 0
+num_aluno = 0
+
+while quantidade <= 0:
+    entrada = input("Quantidade de turmas: ")
+    eh_numero = 1
+    indice = 0
+
+    while indice < 100 and entrada[indice:indice+1]:
+        if entrada[indice] not in "0123456789":
+            eh_numero = 0
